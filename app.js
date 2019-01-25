94aa01aa73145f3986fc5e23cf620b598d3daa7c
wodzynski <macko5395@gmail.com>
2019-01-25 22:35:04 +0100

    EDIT and UPDATE routes working

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express     = require('express'),
-      app         = express(),
-      bodyParser  = require('body-parser'),
-      mongoose    = require('mongoose');
+const express         = require('express'),
+      app             = express(),
+      bodyParser      = require('body-parser'),
+      mongoose        = require('mongoose');
+      methodOverride  = require('method-override');
 
 // APP CONFIG
 mongoose.connect('/mongodb://localhost:27017/restful_blog_app', {useNewUrlParser: true});
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -57,6 +59,28 @@ app.get('/blogs/:id', (req, res) => {
       res.redirect('/blogs');
     } else {
       res.render('show', {blog: foundBlog});
+    }
+  });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if(err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUT
+app.put('/blogs/:id', (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if(err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/' + req.params.id);
     }
   });
 });
