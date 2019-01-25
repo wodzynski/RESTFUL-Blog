b57748379612db8b170685515c9fd75fe43a51de
wodzynski <macko5395@gmail.com>
2019-01-26 00:36:23 +0100

    DELETE route added with delete end edit buttons

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express         = require('express'),
 
 // APP CONFIG
 mongoose.connect('/mongodb://localhost:27017/restful_blog_app', {useNewUrlParser: true});
+mongoose.set("useFindAndModify", false);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -74,8 +75,9 @@ app.get('/blogs/:id/edit', (req, res) => {
   });
 });
 
-// UPDATE ROUT
+// UPDATE ROUTE
 app.put('/blogs/:id', (req, res) => {
+  // Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
     if(err) {
       res.redirect('/blogs');
@@ -83,6 +85,19 @@ app.put('/blogs/:id', (req, res) => {
       res.redirect('/blogs/' + req.params.id);
     }
   });
+});
+
+// DELETE ROUTE
+app.delete('/blogs/:id', (req, res) => {
+  // destroy blog
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if(err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs');
+    }
+  })
+  // redirect somewhere
 });
 
 app.listen(3000, process.env.IP, () => console.log('The server has started working!'));
